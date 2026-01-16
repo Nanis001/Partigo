ddd68baab8893118e888194e9229eed9ee96e766
Nanis-Partigo-01 <Nanispartigo01@gmail.com>
2026-01-16 15:58:17 +0530

    Second Commit

--- JobGiverMobile/app/(auth)/login.tsx
+++ JobGiverMobile/app/(auth)/login.tsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Login Screen</Text>
-      <Link href="/register">Go to Register</Link>
+      <Link href="/register">Go to Register!!!</Link>
       <Button title="Login" onPress={() => {}} />
     </View>
   );
